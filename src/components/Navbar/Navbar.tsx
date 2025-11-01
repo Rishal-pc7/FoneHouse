730e4ba9dd0c42dfd2bfa8865303ba8ab47a5ea4
Rishal-pc7 <rishalpc14@gmail.com>
2025-11-02 03:41:38 +0530

    Update Navbar component to hide logo on mobile view

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
         <Image src={Logo} alt='Fone House' className='object-contain'/>
       </div>
     <div className="fixed right-1/2 translate-x-1/2 min-w-[70%] md:min-w-[25%] h-10 md:h-10 md:px-10  backdrop-blur-lg bg-white/5 border border-white/30 rounded-xl shadow-lg  flex justify-between md:justify-center">
-      <div className="flex logo h-full  w-1/2 md:w-[35%] justify-center md:justify-start items-center   p-3">
+      <div className="flex md:hidden logo h-full  w-1/2 md:w-[35%] justify-center md:justify-start items-center   p-3">
         <Image src={Logo} alt='Fone House' className='object-contain'/>
       </div>
       
